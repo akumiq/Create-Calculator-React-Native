a0933522c061e21ca28a41a7698139e14da42a11
Miqdad <kun.miqdad23@gmail.com>
2020-09-23 09:25:02 +0700

    initial setup for apk creation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Dimensions, View, Text} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const App = () => {
+  const entireScreenWidth = Dimensions.get('window').width;
+  EStyleSheet.build({$rem: entireScreenWidth / 320});
+
+  return (
+    <View>
+      <Text>First Screen</Text>
+    </View>
+  );
+};
+
+export default App;
