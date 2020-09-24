b04619dbfaf492e562b9b79dea32d946fcf5ed6e
Miqdad <kun.miqdad23@gmail.com>
2020-09-24 10:11:42 +0700

    create button layout design

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,18 +1,53 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const DashboardScreen = () => {
+  let numbers = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    ['.', 0, '='],
+  ];
+  let rowNumber = [];
+
+  for (let i = 0; i < 4; i++) {
+    let rows = [];
+    for (let j = 0; j < 3; j++) {
+      rows.push(
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.textBtn}>{numbers[i][j]}</Text>
+        </TouchableOpacity>,
+      );
+    }
+    rowNumber.push(<View style={styles.rowNumber}>{rows}</View>);
+  }
+
+  let operation = ['+', '-', '*', '/'];
+  let operationSign = [];
+
+  for (let i = 0; i < 4; i++) {
+    operationSign.push(
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.textBtn}>{operation[i]}</Text>
+      </TouchableOpacity>,
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.operationCal} />
+      <View style={styles.operationCal}>
+        <Text style={styles.textOperationCal}>11*11</Text>
+      </View>
 
-      <View style={styles.resultCal} />
+      <View style={styles.resultCal}>
+        <Text style={styles.textResultCal}>121</Text>
+      </View>
 
       <View style={styles.wrapperButtons}>
-        <View style={styles.numberCal} />
+        <View style={styles.numberCal}>{rowNumber}</View>
 
-        <View style={styles.operationSign} />
+        <View style={styles.operationSign}>{operationSign}</View>
       </View>
     </View>
   );
@@ -27,10 +62,22 @@ const styles = EStyleSheet.create({
   operationCal: {
     flex: 2,
     backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingRight: '10rem',
+  },
+  textOperationCal: {
+    fontSize: '30rem',
   },
   resultCal: {
     flex: 1,
     backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingRight: '10rem',
+  },
+  textResultCal: {
+    fontSize: '20rem',
   },
   wrapperButtons: {
     flex: 7,
@@ -40,8 +87,26 @@ const styles = EStyleSheet.create({
     flex: 3,
     backgroundColor: 'yellow',
   },
+  rowNumber: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textBtn: {
+    fontSize: '25rem',
+    paddingVertical: '10rem',
+    paddingHorizontal: '20rem',
+  },
   operationSign: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'grey',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
 });
